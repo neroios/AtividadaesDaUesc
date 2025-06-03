94a56a95d973a7ea89cd1d43f58b6025693d0496
Guilherme M Xavier <guilhermex.nero@gmail.com>
2025-06-03 20:30:09 -0300

    Versao pra 5D

--- GitHub/GitHonda/Atividades/TamanhoDeArray.c
+++ GitHub/GitHonda/Atividades/TamanhoDeArray.c
@@ -6,69 +6,116 @@
 int main(){
 
     clock_t inicio = clock();
-    int soma = 0;
-    int x = 10, y = 500;
-    int myArray[x][y];
     double tempo = 0;
-    int k = 100000;
+    int tamanho;
 
-    for (int l = 0; l < k; l++){
-            printf("Lenght = %ld", sizeof(myArray));
-            printf("\n");
-            for(int i = 0; i < x; i++){
-                for(int j = 0; j < y; j++)
-                    myArray[i][j] = i + j;
+    printf("Qual o tamanho: ");
+    scanf("%d", &tamanho);
+
+
+    switch (tamanho) {
+        case 1:
+            int Array[10];
+            for (int i = 0; i < 10; i++) {
+                Array[i] = ++i;
             }
-            printf("\n");
 
-             for(int i = 0; i < x; i++){
-               for(int j = 0; j < y; j++)
-                     printf("%d ", myArray[i][j]);
-
-                 printf("\n");
-             }
-
-            for(int i = 0; i < x; i++){
-                for(int j = 0; j < y; j++)
-                    soma += myArray[i][j];
+            for (int i = 0; i < 10; i++) {
+                printf("%d ", Array[i]);
             }
-            printf("\n");
-            printf("%d", soma);
-            printf("\n");
+            break;
+
+        case 2:
+            int meuArray[10][10];
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    meuArray[i][j] = j+i;
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    printf("%d ", meuArray[i][j]);
+                }
+            }
+                break;
+
+        case 3:
+            int minhaArray[10][10][10];
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                                minhaArray[i][j][k] = j+k+i;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                                printf("%d ", minhaArray[i][j][k]);
+                    }
+                }
+            }
+            break;
+
+        case 4:
+            int mineArray[10][10][10][10];
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        for (int l = 0; l < 10; l++) {
+                                mineArray[i][j][k][l] = j+k+i+l;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        for (int l = 0; l < 10; l++) {
+                                printf("%d ", mineArray[i][j][k][l]);
+                        }
+                    }
+                }
+            }
+            break;
+        case 5:
+
+            int myArray[10][10][10][10][10];
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        for (int l = 0; l < 10; l++) {
+                            for (int n = 0; n < 10; n++) {
+                                myArray[i][j][k][l][n] = j+k+i+l+n;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        for (int l = 0; l < 10; l++) {
+                            for (int n = 0; n < 10; n++) {
+                                printf("%d ", myArray[i][j][k][l][n]);
+                            }
+                        }
+                    }
+                }
+            }
+            break;
     }
+
 
     clock_t fim = clock();
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("Elapsed time: %f seconds\n", tempo / k);
+    printf("\nElapsed time: %f seconds\n", tempo);
 
     return 0;
 }
-
-/*
-5 tries
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-x = 100, y = 50;
-Elapsed time: 0.380000 seconds
-
-x = 50; y = 100;
-Elapsed time: 0.365400 seconds
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-20 tries
-x = 100; y = 50;
-Elapsed time: 0.371450 seconds
-
-x = 50; y = 100;
-Elapsed time: 0.361150 seconds
-
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-1000 tries
-x = 100; y = 50;
-Elapsed time: 0.360939 seconds
-
-x = 50; y = 100;
-Elapsed time: 0.356667 seconds
-
--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-*/
